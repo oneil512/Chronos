89f4983ee54438bec548469b21fc37d7df63f29c
Clay O'Neil <oneil512@umn.edu>
2022-07-31 18:21:41 -0500

    execute code until exception is hit

--- chronos/main.py
+++ chronos/main.py
@@ -9,6 +9,10 @@ import click
 
 FUNCTION_DEF_KEY_SUFFIX = "FUNC_DEF"
 
+STEP_OVER = "o"
+STEP_INTO = "i"
+STEP_BACK = "b"
+
 logger = logging.getLogger(__name__)
 
 
@@ -28,68 +32,96 @@ class Debugger:
 
         self._run(current_code, stack)
 
+    def get_executable_str(self, node: Type[ast.mod], code_lines: List[str]) -> str:
+        executable_str = ""
+
+        line_start = node.lineno - 1
+        line_end = node.end_lineno
+        col_offset = end_col_offset = 0
+
+        for i in range(line_start, line_end):
+
+            if i == line_start:
+                col_offset = node.col_offset
+            if i == line_end - 1:
+                end_col_offset = node.end_col_offset
+
+            end = end_col_offset + 1 if end_col_offset else len(code_lines[i])
+            executable_str += code_lines[i][col_offset:end] + "\n"
+            col_offset = end_col_offset = 0
+
+        return executable_str
+
     def _run(self, current_code: str, stack: List[Dict]) -> None:
         """Parse code into tree and execute whole tree."""
+
         try:
             syntax_tree = ast.parse(current_code)
         except ValueError as e:
             raise SystemError(f"Cannot parse into abstract syntax tree, {e}")
 
-        code_lines = current_code.splitlines()
         ast_stack = list(reversed(syntax_tree.body))
-
         self.states_across_time.append((copy.deepcopy(ast_stack), copy.deepcopy(stack)))
 
+        # Execute code until exception
         while ast_stack:
+
             node = ast_stack.pop()
+            executable_str = self.get_executable_str(node, current_code.splitlines())
 
-            executable_str = ""
-
-            line_start = node.lineno - 1
-            line_end = node.end_lineno
-            col_offset = end_col_offset = 0
-
-            for i in range(line_start, line_end):
-
-                if i == line_start:
-                    col_offset = node.col_offset
-                if i == line_end - 1:
-                    end_col_offset = node.end_col_offset
-
-                end = end_col_offset + 1 if end_col_offset else len(code_lines[i])
-                executable_str += code_lines[i][col_offset:end] + "\n"
-                col_offset = end_col_offset = 0
-
-            self.print_code(executable_str)
-            step = click.prompt("Enter: o, i, b, out", type=str)
-
+            # Save function def in stack
             if type(node) is ast.FunctionDef:
-                if not stack[-1].get("locals"):
-                    stack[-1]["locals"] = defaultdict()
-
                 stack[-1]["locals"][
                     node.name + FUNCTION_DEF_KEY_SUFFIX
                 ] = executable_str
 
-            if step.lower().strip() == "b":
-                self.states_across_time.pop()
-                ast_stack, stack = self.states_across_time.pop()
+            try:
+                frame = self._step_over(executable_str, stack)
+                stack.append(frame)
+
+                self.states_across_time.append(
+                    (copy.deepcopy(ast_stack), copy.deepcopy(stack))
+                )
                 continue
+            except Exception as e:
+                print(f"Encountered exception {e}. Starting interactive session.")
+                self.interactive_session(ast_stack, current_code, stack)
 
-            if step.lower().strip() == "i":
+    def interactive_session(
+        self, ast_stack: List[Type[ast.mod]], current_code: str, stack: List[Dict]
+    ) -> None:
+        """Get instructions from user and execute them."""
+        while ast_stack:
+            node = ast_stack.pop()
+            executable_str = self.get_executable_str(node, current_code.splitlines())
+            action = click.prompt("Enter: o, i, b, out", type=str)
 
+            self.print_code(executable_str)
+
+            # Save function def in stack
+            if type(node) is ast.FunctionDef:
+                stack[-1]["locals"][
+                    node.name + FUNCTION_DEF_KEY_SUFFIX
+                ] = executable_str
+
+            # Step back
+            if action.lower().strip() == STEP_BACK:
+                ast_stack, stack = self._step_back()
+
+            # Step into
+            if action.lower().strip() == STEP_INTO:
                 if type(node.value) is ast.Call:
-
                     inner_ast, frame = self._step_into(node, stack)
                     ast_stack.extend(reversed(inner_ast))
-
                 else:
                     print("Can't step into non function call. Stepping over.")
-                    step = "o"
+                    action = STEP_OVER
 
-            if step.lower().strip() == "o":
-                frame = self._run_executor(executable_str.strip(), stack.pop())
+            # Step over
+            if action.lower().strip() == STEP_OVER:
+                frame = self._step_over(executable_str, stack)
 
+            # Update states
             if frame:
                 stack.append(frame)
 
@@ -114,9 +146,19 @@ class Debugger:
 
         return frame
 
+    def _step_back(self) -> Tuple(List[Type[ast.mod]], List[Dict]):
+        """Step back to the previous code and state."""
+        self.states_across_time.pop()
+        ast_stack, stack = self.states_across_time.pop()
+        return ast_stack, stack
+
+    def _step_over(self, executable_str: str, stack: List[Dict]) -> Dict:
+        """Execute current line."""
+        return self._run_executor(executable_str.strip(), stack.pop())
+
     def _step_into(
         self, node: Type[ast.mod], stack: List[Dict]
-    ) -> Tuple(Type[ast.AST, Dict]):
+    ) -> Tuple(Type[ast.AST], Dict):
         """Step into a new call frame."""
         prev_frame = stack[-1]
         func_name = node.value.func.id + FUNCTION_DEF_KEY_SUFFIX
